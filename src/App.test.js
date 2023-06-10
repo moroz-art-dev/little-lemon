10033ac698a47921d7e05e5262bca7578f7491fd
moroz-art-dev <moroz.art.dev@gmail.com>
2023-06-10 15:13:45 +0300

    update styles and test

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,7 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
+import { describe, test, expect } from '@jest/globals';
 import App from './App';
 
 describe('App component', () => {
